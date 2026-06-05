4ed28971dd7b2014e6e583ee67ccba7b5fa35553
t-hallo <sense@tuta.com>
2026-06-05 08:51:57 +0800

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,6 +2,6 @@ module.exports = {
   title: "WORTH.EE",
   description: "Archive Light — Anti-conventional structure",
   url: "https://worth.ee",
-  email: "hello@worth.ee",
+  email: "i@worth.ee",
   location: "SYS_LOC: ESTONIA // AREA_ID: 59.4370° N, 24.7536° E"
 };
